28b82b8ce5d3b5205935c94778d6272828c76e87
Natalya <alkinatalya@yandex.com>
2023-02-14 18:05:14 +0300

    add task2

---
+++ task2/main.py
@@ -0,0 +1,41 @@
+import actions_with as act
+from telegram import Update
+from telegram.ext import  ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+import user_input as u
+
+
+heap = {
+    'count': 0,
+    'first': None,
+    'second': None,
+    'operator': None
+}
+
+async def startcalc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    heap['count']=1
+    await update.message.reply_text("Вводи пример")
+    
+async def calculate(update: Update, contex: ContextTypes.DEFAULT_TYPE) -> None:
+    if heap['count'] != 0:
+        if heap['count'] == 1:
+            heap['first'] = update.message.text
+            heap['count'] +=1
+        elif heap['count'] == 2:
+            heap['second'] = update.message.text
+            heap['count'] +=1
+        elif heap['count'] == 3:
+            heap['operator'] = update.message.text
+            a,b = u.inp(heap['first'], heap['second'])
+            await update.message.reply_text(f"{a} {heap['operator']} {b} = {act.action(a,b,heap['operator'])}")
+            heap['count'] = 0
+            heap['first'] = None
+            heap['second'] = None
+            heap['operator'] = None
+            
+app = ApplicationBuilder().token("TOKEN").build()
+
+app.add_handler(CommandHandler("start", startcalc))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, calculate))
+
+
+app.run_polling()
